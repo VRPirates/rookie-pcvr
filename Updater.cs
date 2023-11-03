88cceb5da77421cebf2f6c0810c0612247a00a30
fenopy <fenopie@gmail.com>
2023-11-03 12:49:01 -0500

    chore: version bump

--- Updater.cs
+++ Updater.cs
@@ -11,7 +11,7 @@ namespace RookiePCVR
         private static string RawGitHubUrl;
         private static string GitHubUrl;
 
-        public static readonly string LocalVersion = "1.2";
+        public static readonly string LocalVersion = "1.3";
         public static string currentVersion = string.Empty;
         public static string changelog = string.Empty;
 
